45b7dc25ab2bfc4d4c235a456fc2cfc903b9724a
Ryan Muskopf (RIT Student) <rjm9187@ad.rit.edu>
2017-03-11 16:20:06 -0500

    users can now enter a new room of their choice by name

--- src/server.js
+++ src/server.js
@@ -28,38 +28,39 @@ const rooms = {
   },
 };
 
-const join = (sock) => {
+const listeners = (sock) => {
   const socket = sock;
-  socket.roomToJoin = {};
-
-  let makeNew = true;
-
-  const keys = Object.keys(rooms);
-
-  for (let i = 0; i < keys.length; i++) {
-    const room = rooms[keys[i]];
-
-    if (room.userNum < 4) {
-      socket.roomToJoin = room.title;
-      makeNew = false;
-    }
-  }
-
-  if (makeNew) {
-    roomNum++;
-    console.log(`creating room${roomNum}`);
-
-    rooms[`room${roomNum}`] = {
-      title: `room${roomNum}`,
-      userNum: 0,
-    };
-
-    console.dir(rooms[`room${roomNum}`]);
-
-    socket.roomToJoin = rooms[`room${roomNum}`].title;
-  }
 
   socket.on('join', () => {
+		socket.roomToJoin = {};
+
+  	let makeNew = true;
+	
+  	const keys = Object.keys(rooms);
+	
+  	for (let i = 0; i < keys.length; i++) {
+  	  const room = rooms[keys[i]];
+	
+  	  if (room.userNum < 4) {
+  	    socket.roomToJoin = room.title;
+  	    makeNew = false;
+  	  }
+  	}
+	
+  	if (makeNew) {
+  	  roomNum++;
+  	  console.log(`creating room${roomNum}`);
+	
+  	  rooms[`room${roomNum}`] = {
+  	    title: `room${roomNum}`,
+  	    userNum: 0,
+  	  };
+	
+  	  console.dir(rooms[`room${roomNum}`]);
+	
+  	  socket.roomToJoin = rooms[`room${roomNum}`].title;
+  	}
+		
     rooms[socket.roomToJoin].userNum++;
     console.log(`Joining room ${socket.roomToJoin}. Users:${rooms[socket.roomToJoin].userNum} `);
 
@@ -69,12 +70,44 @@ const join = (sock) => {
 
     socket.broadcast.to(socket.roomToJoin).emit('requestCanvas');
   });
-};
+	
+	socket.on('changeRoom', (data) => {
+		let makeNew = true;
+		
+		const keys = Object.keys(rooms);
+	
+  	for (let i = 0; i < keys.length; i++) {
+  	  const room = rooms[keys[i]];
+	
+  	  if (room.title == data.newRoom) {
+  	    socket.roomToJoin = room.title;
+  	    makeNew = false;
+  	  }
+  	}
+		
+		if (makeNew) {
+  	  console.log(`creating room ${data.newRoom}`);
+	
+  	  rooms[data.newRoom] = {
+  	    title: data.newRoom,
+  	    userNum: 0,
+  	  };
+	
+  	  console.dir(rooms[data.newRoom]);
+	
+  	  socket.roomToJoin = rooms[data.newRoom].title;
+  	}
+		
+		rooms[socket.roomToJoin].userNum++;
+    console.log(`Joining room ${socket.roomToJoin}. Users:${rooms[socket.roomToJoin].userNum} `);
 
-const listeners = (sock) => {
-  const socket = sock;
+    socket.join(socket.roomToJoin);
 
-  join(socket);
+    socket.emit('roomNum', socket.roomToJoin);
+
+    socket.broadcast.to(socket.roomToJoin).emit('requestCanvas');
+		
+	});
 
   socket.on('draw', (data) => {
     socket.broadcast.to(socket.roomToJoin).emit('recieveDraw', data);
